c9f0bc321230961b52ab369f8e0ff2823fc9bce0
[kuromi] <[merongmerong@gmail.com]>
2024-08-07 21:09:55 +0900

    Modify ui.py to add le1, ld2 and cb widgets

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from ctrl import Control
 from PyQt5.QtWidgets import QApplication
 
 # PyQt5의 Qt 플랫폼 플러그인 경로를 지정합니다.
-qt_plugin_path = r'C:\calculator\platforms'  # Qt 플랫폼 플러그인 경로
+qt_plugin_path = r'C:\calculator_A\platforms'  # Qt 플랫폼 플러그인 경로
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = qt_plugin_path
 
 def main():
--- ui.py
+++ ui.py
@@ -1,17 +1,24 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
-                             QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel)
+                             QMessageBox, QPlainTextEdit, QHBoxLayout,QLineEdit,QComboBox)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDate, Qt
+from PyQt5 import QtCore
 
 class View(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.date=QDate.currentDate()
         self.initUI()
 
     def initUI(self):
-        self.lbl1= QLabel(self.date.toString(Qt.DefaultLocaleLongDate), self)
+        self.le1=QLineEdit('0',self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+
+        self.le2=QLineEdit('0',self)
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
+
+        self.cb= QComboBox(self)
+        self.cb.addItems(['+','-','*','/'])
+
         self.te1=QPlainTextEdit()
         self.te1.setReadOnly(True)
 
@@ -21,6 +28,11 @@ class View(QWidget):
         self.btn2=QPushButton('Clear',self)
         self.btn2.clicked.connect(self.clearMessage)
 
+        hbox_formular=QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+
         hbox=QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
@@ -29,9 +41,9 @@ class View(QWidget):
         vbox=QVBoxLayout()
         vbox.addWidget(self.te1)
         #vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox_formular)
         vbox.addLayout(hbox)
-        #vbox.addStretch(1)
-        vbox.addWidget(self.lbl1)
+        vbox.addStretch(1)
 
         self.setLayout(vbox)
 
